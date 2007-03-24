69b4e451a461d8ac3b90036bc425c7c290946b2f
Alex Rau <alex@addresspush.com>
2007-03-24 19:53:29 +0000

    Added  IF_ICMPNE -> IF_ICMPEQ with unit tests. MutationCreator returns now multiple first level mutants.

--- Mutation/src/main/java/mut/mutantgen/bcel/BCELMutantCreator.java
+++ Mutation/src/main/java/mut/mutantgen/bcel/BCELMutantCreator.java
@@ -1,13 +1,16 @@
 package mut.mutantgen.bcel;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
-import org.apache.bcel.generic.BIPUSH;
 import org.apache.bcel.generic.ClassGen;
-import org.apache.bcel.generic.IRETURN;
+import org.apache.bcel.generic.IF_ICMPEQ;
+import org.apache.bcel.generic.IF_ICMPNE;
 import org.apache.bcel.generic.Instruction;
 import org.apache.bcel.generic.InstructionHandle;
 import org.apache.bcel.generic.InstructionList;
@@ -15,7 +18,16 @@ import org.apache.bcel.generic.MethodGen;
 import org.apache.bcel.util.InstructionFinder;
 
 public class BCELMutantCreator {
-	
+
+	public List<byte[]> getModifiedByteCodesForClass(String classToBeModified,
+			String methodToBeModified) throws ClassNotFoundException {
+
+		ArrayList<byte[]> byteCodeResults = new ArrayList<byte[]>();
+
+		return byteCodeResults;
+
+	}
+
 	public byte[] getModifiedByteCodeForClass(String classToBeModified,
 			String methodToBeModified) throws ClassNotFoundException {
 
@@ -36,28 +48,31 @@ public class BCELMutantCreator {
 		InstructionList instructions = new InstructionList(methodGet42
 				.getCode().getCode());
 
-		InstructionFinder finder = new InstructionFinder(instructions);
-
-		Iterator instructionIterator = finder.search("BIPUSH");
-
-		while (instructionIterator.hasNext()) {
-			InstructionHandle[] handles = (InstructionHandle[]) instructionIterator
-					.next();
-			for (int i = 0; i < handles.length; i++) {
-
-				Instruction instruction = handles[i].getInstruction();
-				System.out.println("Instruction: " + instruction);
-				handles[i].swapInstruction(new BIPUSH(new Integer(41)
-						.byteValue()));
-
-			}
-
-		}
-
-		instructions.append(new IRETURN());
+		// InstructionFinder finder = new InstructionFinder(instructions);
+		//
+		// Iterator instructionIterator = finder.search("BIPUSH");
+		//
+		// while (instructionIterator.hasNext()) {
+		// InstructionHandle[] handles = (InstructionHandle[])
+		// instructionIterator
+		// .next();
+		// for (int i = 0; i < handles.length; i++) {
+		//
+		// Instruction instruction = handles[i].getInstruction();
+		// System.out.println("Instruction: " + instruction);
+		// handles[i].swapInstruction(new BIPUSH(new Integer(41)
+		// .byteValue()));
+		//
+		// }
+		//
+		// }
+		//
+		// instructions.append(new IRETURN());
+		//		
 
 		MethodGen methodGen = new MethodGen(methodGet42, clazz.getClassName(),
 				classGen.getConstantPool());
+		methodGen.setMaxStack();
 		methodGen.setInstructionList(instructions);
 
 		classGen.replaceMethod(methodGet42, methodGen.getMethod());
@@ -67,4 +82,54 @@ public class BCELMutantCreator {
 		return classGen.getJavaClass().getBytes();
 	}
 
+	public byte[][] getModifiedByteCodeForClass(JavaClass clazz,
+			java.lang.reflect.Method method, String byteCodeInstruction)
+			throws ClassNotFoundException {
+
+		Method bcelMethod = clazz.getMethod(method);
+
+		InstructionList instructions = new InstructionList(bcelMethod.getCode()
+				.getCode());
+
+		InstructionFinder finder = new InstructionFinder(instructions);
+
+		Iterator instructionIterator = finder.search(byteCodeInstruction);
+
+		Vector<byte[]> byteCodeModifiedClasses = new Vector<byte[]>();
+
+		while (instructionIterator.hasNext()) {
+
+			InstructionHandle[] handles = (InstructionHandle[]) instructionIterator
+					.next();
+
+			handles[0].setInstruction(((IF_ICMPNE) handles[0].getInstruction())
+					.negate());
+
+			// instructions.setPositions();
+
+			ClassGen classGen = new ClassGen(clazz);
+
+			MethodGen methodGen = new MethodGen(bcelMethod, clazz
+					.getClassName(), classGen.getConstantPool());
+
+			methodGen.setInstructionList(instructions);
+
+			classGen.replaceMethod(bcelMethod, methodGen.getMethod());
+
+			// methodGen.setMaxLocals();
+			// methodGen.setMaxStack();
+
+			byteCodeModifiedClasses.add(classGen.getJavaClass().getBytes());
+
+			handles[0].setInstruction(((IF_ICMPEQ) handles[0].getInstruction())
+					.negate());
+
+			// instructions.setPositions();
+
+		}
+
+		instructions.dispose();
+
+		return byteCodeModifiedClasses.toArray(new byte[0][]);
+	}
 }
