bcb5f2ecdab97dc6855beaefd718aa748007fd46
Alex Rau <alex@addresspush.com>
2007-03-25 12:27:09 +0000

    Added code for creating log file if it does not exist already

--- Mutation/src/main/java/mut/log/FileEventLogger.java
+++ Mutation/src/main/java/mut/log/FileEventLogger.java
@@ -7,24 +7,32 @@ import java.util.Date;
 
 public class FileEventLogger implements IEventLogger {
 	private FileWriter fw;
-	public FileEventLogger(String fileName) throws IOException{
+
+	public FileEventLogger(String fileName) throws IOException {
 		File file = new File(fileName);
-		fw = new FileWriter(fileName);
-		System.out.println("# Writing logs to: " +file.getAbsolutePath());
-		
+
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		fw = new FileWriter(file);
+		System.out.println("# Writing logs to: " + file.getAbsolutePath());
+
 	}
+
 	public void log(Event eventType, Object message) {
-		String msg = "" +new Date() +" - "  +eventType +": " +message +"\n";
+		String msg = "" + new Date() + " - " + eventType + ": " + message
+				+ "\n";
 		try {
-			
+
 			fw.write(msg);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
 	}
-	
-	public void close(){
+
+	public void close() {
 		try {
 			fw.close();
 		} catch (IOException e) {
