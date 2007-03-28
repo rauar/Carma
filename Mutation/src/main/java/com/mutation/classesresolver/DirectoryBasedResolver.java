a1385e2ca8b27a142cf8281a781a650bd2dae002
Mike Grözinger <mike.groezinger@siobra.de>
2007-03-28 22:09:40 +0000

    - added classes and test set resolvers

---
+++ Mutation/src/main/java/com/mutation/classesresolver/DirectoryBasedResolver.java
@@ -0,0 +1,66 @@
+package com.mutation.classesresolver;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.mutation.IClassSetResolver;
+import com.mutation.events.IEventListener;
+
+/**
+ * determines set of all classes within a directory of class files
+ * @author mike
+ *
+ */
+public class DirectoryBasedResolver implements IClassSetResolver {
+	/**
+	 * base directory for classes
+	 */
+	private File classesBaseDir;
+
+	public Set<String> determineClassNames(IEventListener eventListener) {
+		Set<String> classNames = new HashSet<String>();
+		iterate(this.classesBaseDir, "", "", classNames);
+		return classNames;
+	}
+
+	public void setClassesBaseDir(File classesBaseDir) {
+		this.classesBaseDir = classesBaseDir;
+	}
+
+	/**
+	 * iterate recursively over directory and add all class names of .class files as classes
+	 * @param baseDir starting directory
+	 * @param packagePrefix prefix for that directory
+	 * @param relPath relative path from basedir
+	 * @param classNames set of class names to add classes found
+	 */
+	void iterate(File baseDir, String packagePrefix, String relPath,
+			Set<String> classNames) {
+
+		File[] files = baseDir.listFiles();
+		if(files == null){
+			return;
+		}
+		
+		for (File file : files) {
+			if (file.isDirectory()) {
+				String prefix = packagePrefix.equals("") ? file.getName()
+						: packagePrefix + "." + file.getName();
+				String relSubPath = relPath + "/" + file.getName();
+				iterate(file, prefix, relSubPath, classNames);
+			} else {
+				String fileName = file.getName();
+
+				if (fileName.endsWith(".class")) {
+					String relClassName = fileName.substring(0, fileName
+							.length()
+							- ".class".length());
+					String className = packagePrefix + "." + relClassName;
+					classNames.add(className);
+				}
+			}
+		}
+
+	}	
+}
---
+++ Mutation/src/main/java/com/mutation/testsetresolver/OneTestPerClassResolver.java
@@ -0,0 +1,26 @@
+package com.mutation.testsetresolver;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.mutation.ITestSetResolver;
+import com.mutation.events.IEventListener;
+
+/**
+ * determines test set as a single test per class using a suffix naming convention.
+ * @author mike
+ *
+ */
+public class OneTestPerClassResolver implements ITestSetResolver {
+	private String testCaseSuffix;
+	public Set<String> determineTests(String classUnderTestName, IEventListener eventListener) {
+		String testClassName = classUnderTestName + this.testCaseSuffix;
+		Set<String> tests = new HashSet<String>();
+		tests.add(testClassName);
+		return tests;
+	}
+	public void setTestCaseSuffix(String testCaseSuffix) {
+		this.testCaseSuffix = testCaseSuffix;
+	}
+
+}
