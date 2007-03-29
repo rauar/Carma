341615d82a4bab2ac32e51d019c09f347854b064
Mike Grözinger <mike.groezinger@siobra.de>
2007-03-29 00:06:01 +0000

    added mutant sum

--- Mutation/src/main/java/com/mutation/events/listener/ConsoleEventListener.java
+++ Mutation/src/main/java/com/mutation/events/listener/ConsoleEventListener.java
@@ -1,17 +1,23 @@
 package com.mutation.events.listener;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import com.mutation.Mutant;
 import com.mutation.events.DriverFinished;
 import com.mutation.events.DriverStarted;
 import com.mutation.events.IEvent;
 import com.mutation.events.IEventListener;
+import com.mutation.events.MutantsGenerated;
 import com.mutation.events.TestsExecuted;
 import com.mutation.util.StopWatch;
 
 public class ConsoleEventListener  implements IEventListener {
 	int numSurvivers;
 	int numKilled;
+	Set<Mutant> totalMutants = new HashSet<Mutant>();
+	Set<Mutant> suvivers = new HashSet<Mutant>();
 	private StopWatch watch = new StopWatch();
 	public void notifyEvent(IEvent event) {
 		System.out.println(new Date() +": "  +event);
@@ -21,13 +27,19 @@ public class ConsoleEventListener  implements IEventListener {
 			double elapsed = watch.stop();
 			System.out.println("# Finished. total time: " +elapsed/1000 +" sec.");
 			System.out.println("# numSurvivers: " +numSurvivers +" numKilled: " +numKilled);
+			System.out.println("# numSurvivers: " +suvivers.size() +" totalNumMutants: " +totalMutants.size());
 		}else if(event instanceof TestsExecuted){
 			TestsExecuted te = (TestsExecuted) event;
 			if(te.isMutantSurvived()){
 				numSurvivers ++;
 			}else{
+				suvivers.remove(te.getMutant());
 				numKilled++;
 			}
+		}else if(event instanceof MutantsGenerated){
+			MutantsGenerated e = (MutantsGenerated)event;
+			totalMutants.addAll(e.getGenerateMutants());
+			suvivers.addAll(e.getGenerateMutants());
 		}
 	}
 
