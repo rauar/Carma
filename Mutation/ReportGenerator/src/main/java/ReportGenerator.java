f6a547ab27d4ef9e7100daf50327e907dd57b7e6
Alex Rau <alex@addresspush.com>
2007-04-29 12:46:53 +0000

    Some cleanup

--- Mutation/ReportGenerator/src/main/java/ReportGenerator.java
+++ Mutation/ReportGenerator/src/main/java/ReportGenerator.java
@@ -8,7 +8,6 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
-import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
@@ -17,10 +16,34 @@ import com.mutation.report.om.MutationRun;
 
 public class ReportGenerator {
 
+	private String source;
+
+	private String destination;
+
+	private String template;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		if (args.length != 3) {
+			System.out.println("Use arguments: source, destination, template");
+			System.exit(-1);
+		}
+
+		new ReportGenerator(args[0], args[1], args[2]).perform();
+
+	}
+
+	public ReportGenerator(String source, String destination, String template) {
+		super();
+		this.source = source;
+		this.destination = destination;
+		this.template = template;
+	}
+
+	public void perform() {
 
 		try {
 			VelocityEngine engine = new VelocityEngine();
@@ -28,20 +51,17 @@ public class ReportGenerator {
 
 			JAXBContext context = JAXBContext.newInstance(MutationRun.class);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			MutationRun report = (MutationRun) unmarshaller.unmarshal(new FileInputStream(new File(
-					"src/main/resources/report.xml")));
-
-			Template reportTemplate = engine.getTemplate("src/main/resources/report.vm");
+			MutationRun report = (MutationRun) unmarshaller.unmarshal(new FileInputStream(new File(source)));
 
 			VelocityContext vcontext = new VelocityContext();
 			vcontext.put("report", report);
 
 			StringWriter w = new StringWriter();
 
-			Velocity.mergeTemplate("src/main/resources/report.vm", "UTF-8", vcontext, w);
+			Velocity.mergeTemplate(template, "UTF-8", vcontext, w);
 			System.out.println(" template : " + w);
 
-			FileWriter writer = new FileWriter("target/report.html");
+			FileWriter writer = new FileWriter(destination);
 			writer.write(w.toString());
 			writer.close();
 
@@ -50,9 +70,7 @@ public class ReportGenerator {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 }
