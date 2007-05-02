8c4429909e7ae88a8f742bd5d6a4db618c7cc54c
Alex Rau <alex@addresspush.com>
2007-05-02 23:34:34 +0000

    Extended test with more detailed assertions

--- Mutation/MutationRunner/src/test/java/com/mutation/runner/MutationRunnerTestCase.java
+++ Mutation/MutationRunner/src/test/java/com/mutation/runner/MutationRunnerTestCase.java
@@ -38,7 +38,7 @@ public class MutationRunnerTestCase extends TestCase {
 
 	}
 
-	public void test_performMutations() throws Exception {
+	public void test_performMutations_Sample_SampleTestCase() throws Exception {
 
 		TestEventListener listener = new TestEventListener();
 
@@ -47,10 +47,10 @@ public class MutationRunnerTestCase extends TestCase {
 
 		JUnitRunner testRunner = new JUnitRunner();
 		testRunner.setStopOnFirstFailedTest(false);
-		
+
 		List<File> testClassFiles = new ArrayList<File>();
 		testClassFiles.add(new File("src/test/it/it0001/"));
-		
+
 		testRunner.setTestClassesLocationsAsFiles(testClassFiles);
 
 		MutationRunner runner = new MutationRunner();
@@ -66,25 +66,80 @@ public class MutationRunnerTestCase extends TestCase {
 		ByteCodeFileReader bcReader = new ByteCodeFileReader();
 
 		ClassDescription classDescription = new ClassDescription();
-		classDescription.setClassName("sources.Sample");
-		classDescription.setPackageName("");
+		classDescription.setClassName("Sample");
+		classDescription.setPackageName("sources");
 
 		Set<String> testNames = new HashSet<String>();
 		testNames.add("testsources.SampleTestCase");
 
 		runner.performMutations(mutationOperators, bcReader, classDescription, testNames);
 
-		assertEquals(10, listener.eventList.size());
-		assertTrue(listener.eventList.get(0) instanceof ProcessingClassUnderTest);
-		assertTrue(listener.eventList.get(1) instanceof ProcessingMutationOperator);
+		assertEquals("Wrong number of events fired during mutation run", 10, listener.eventList.size());
+
+		assertTrue("Wrong event", listener.eventList.get(0) instanceof ProcessingClassUnderTest);
+		assertEquals("Sample", ((ProcessingClassUnderTest) listener.eventList.get(0)).getClassUnderTest()
+				.getClassName());
+		assertEquals("sources", ((ProcessingClassUnderTest) listener.eventList.get(0)).getClassUnderTest()
+				.getPackageName());
+		assertNull(((ProcessingClassUnderTest) listener.eventList.get(0)).getClassUnderTest().getClassFile());
+
+		assertTrue("Wrong event", listener.eventList.get(1) instanceof ProcessingMutationOperator);
+		assertEquals("ROR", ((ProcessingMutationOperator) listener.eventList.get(1)).getOperatorName());
+
 		assertTrue(listener.eventList.get(2) instanceof MutantsGenerated);
-		assertTrue(listener.eventList.get(3) instanceof ProcessingMutant);
-		assertTrue(listener.eventList.get(4) instanceof TestsExecuted);
-		assertTrue(listener.eventList.get(5) instanceof ProcessingMutant);
-		assertTrue(listener.eventList.get(6) instanceof TestsExecuted);
-		assertTrue(listener.eventList.get(7) instanceof ProcessingMutant);
-		assertTrue(listener.eventList.get(8) instanceof TestsExecuted);
-		assertTrue(listener.eventList.get(9) instanceof ProcessingClassUnderTestFinished);
+		assertEquals("sources.Sample", ((MutantsGenerated) listener.eventList.get(2)).getClassUnderTest());
+		assertEquals(EMutationOperator.ROR, ((MutantsGenerated) listener.eventList.get(2)).getOperator());
+		assertEquals(3, ((MutantsGenerated) listener.eventList.get(2)).getGeneratedMutants().size());
+
+		// TODO: the following three statements should show that the survived
+		// attribute has not been "calculated" yet --> snapshot of current event
+		// which cannot be changed due to later processing steps
+
+		assertFalse(((MutantsGenerated) listener.eventList.get(2)).getGeneratedMutants().get(0).isSurvived());
+		assertFalse(((MutantsGenerated) listener.eventList.get(2)).getGeneratedMutants().get(1).isSurvived());
+		assertFalse(((MutantsGenerated) listener.eventList.get(2)).getGeneratedMutants().get(2).isSurvived());
+		// TODO: investigate more the generated mutants
+
+		assertTrue("Wrong event", listener.eventList.get(3) instanceof ProcessingMutant);
+		assertFalse(((ProcessingMutant) listener.eventList.get(3)).getMutant().isSurvived()); // should
+		// still be
+		// set
+		// as
+		// survived
+
+		assertTrue("Wrong event", listener.eventList.get(4) instanceof TestsExecuted);
+		assertFalse(((TestsExecuted) listener.eventList.get(4)).isMutantSurvived());
+		assertEquals(1, ((TestsExecuted) listener.eventList.get(4)).getKillerTestNames().size());
+		assertEquals("testsources.SampleTestCase", ((TestsExecuted) listener.eventList.get(4)).getKillerTestNames()
+				.iterator().next());
+
+		assertTrue("Wrong event", listener.eventList.get(5) instanceof ProcessingMutant);
+		assertFalse(((ProcessingMutant) listener.eventList.get(5)).getMutant().isSurvived()); // should
+		// still be
+		// set
+		// as
+		// survived
+
+		assertTrue("Wrong event", listener.eventList.get(6) instanceof TestsExecuted);
+		assertFalse(((TestsExecuted) listener.eventList.get(6)).isMutantSurvived());
+		assertEquals(1, ((TestsExecuted) listener.eventList.get(6)).getKillerTestNames().size());
+		assertEquals("testsources.SampleTestCase", ((TestsExecuted) listener.eventList.get(6)).getKillerTestNames()
+				.iterator().next());
+
+		assertTrue("Wrong event", listener.eventList.get(7) instanceof ProcessingMutant);
+		assertFalse(((ProcessingMutant) listener.eventList.get(7)).getMutant().isSurvived()); // should
+		// still be
+		// set
+		// as
+		// survived
+
+		assertTrue("Wrong event", listener.eventList.get(8) instanceof TestsExecuted);
+		assertTrue(((TestsExecuted) listener.eventList.get(8)).isMutantSurvived());
+		assertEquals(1, ((TestsExecuted) listener.eventList.get(8)).getKillerTestNames().size());
+		assertEquals("testsources.SampleTestCase", ((TestsExecuted) listener.eventList.get(8)).getKillerTestNames()
+				.iterator().next());
+
+		assertTrue("Wrong event", listener.eventList.get(9) instanceof ProcessingClassUnderTestFinished);
 	}
 
 }
