13897ea4c6ba470b3c47a2750a098c0aba718cfd
Alex Rau <alex@addresspush.com>
2007-05-02 23:34:14 +0000

    Formatted

--- Mutation/MutationRunner/src/main/java/com/mutation/runner/events/TestsExecuted.java
+++ Mutation/MutationRunner/src/main/java/com/mutation/runner/events/TestsExecuted.java
@@ -6,11 +6,15 @@ import com.mutation.runner.Mutant;
 import com.mutation.runner.utililties.ToStringUtils;
 
 public class TestsExecuted implements IEvent {
-	private Mutant mutant;
-	private Set<String> testNames;
-	private boolean mutantSurvived;
-	private Set<String> killerTestNames;
 	
+	private Mutant mutant;
+
+	private Set<String> testNames;
+
+	private boolean mutantSurvived;
+
+	private Set<String> killerTestNames;
+
 	public TestsExecuted(Mutant mutant, Set<String> testNames, boolean mutantSurvived, Set<String> killerTestNames) {
 		super();
 		this.mutant = mutant;
@@ -18,19 +22,23 @@ public class TestsExecuted implements IEvent {
 		this.mutantSurvived = mutantSurvived;
 		this.killerTestNames = killerTestNames;
 	}
+
 	public Set<String> getKillerTestNames() {
 		return killerTestNames;
 	}
+
 	public Mutant getMutant() {
 		return mutant;
 	}
+
 	public boolean isMutantSurvived() {
 		return mutantSurvived;
 	}
+
 	public Set<String> getTestNames() {
 		return testNames;
 	}
-	
+
 	@Override
 	public String toString() {
 		return ToStringUtils.toString(this);
