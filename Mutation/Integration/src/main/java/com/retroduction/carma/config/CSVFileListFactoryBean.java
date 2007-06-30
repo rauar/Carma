02d7021f4bb2f028654d3e42f9dea5f9d61f652e
Alex Rau <alex@addresspush.com>
2007-06-30 23:11:52 +0000

    Added testcases for Resolver. Removed useless classpath and testclasspath. Fixed CAR-46

---
+++ Mutation/Integration/src/main/java/com/retroduction/carma/config/CSVFileListFactoryBean.java
@@ -0,0 +1,40 @@
+package com.retroduction.carma.config;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.FactoryBean;
+/**
+ * helper class for reading a list of Files from a CSV string
+ * @author mike
+ *
+ */
+public class CSVFileListFactoryBean implements FactoryBean {
+	private List<File> files;
+	public Object getObject() throws Exception {
+		return files;
+	}
+
+	public Class getObjectType() {
+		return List.class;
+	}
+
+	public boolean isSingleton() {
+		return true;
+	}
+
+	
+	public void setCsvNamesList(String csvFiles) throws MalformedURLException{
+		String[] fileNames = csvFiles.split(",");
+		List<File> filesList = new ArrayList<File>( fileNames.length);
+		
+		for(String f : fileNames){
+			filesList.add( new File(f) );
+			
+		}
+		this.files = filesList;
+		
+	}
+}
--- Mutation/JavaApplication/src/main/java/com/retroduction/carma/application/Carma.java
+++ Mutation/JavaApplication/src/main/java/com/retroduction/carma/application/Carma.java
@@ -1,6 +1,5 @@
 package com.retroduction.carma.application;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,8 +8,6 @@ import java.util.Properties;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.ParseException;
 
-import com.retroduction.carma.application.CarmaDriverSetup;
-import com.retroduction.carma.application.CarmaException;
 import com.retroduction.carma.application.util.CLIValidator;
 import com.retroduction.carma.core.Core;
 
@@ -40,7 +37,7 @@ public class Carma {
 		Properties customProps = new Properties();
 		try {
 			customProps.load(new FileInputStream(customPropertiesFile));
-		
+
 		} catch (IOException e) {
 			throw new CarmaException("Failed to load configuration", e);
 		}
