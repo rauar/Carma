25133d9d416a7454a9b509faceebf579bb29c788
Alex Rau <alex@addresspush.com>
2007-10-14 16:53:21 +0000

    Removed empty folder, unused import

--- Mutation/Core/src/main/java/com/retroduction/carma/core/MutantGenerator.java
+++ Mutation/Core/src/main/java/com/retroduction/carma/core/MutantGenerator.java
@@ -2,7 +2,6 @@ package com.retroduction.carma.core;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import com.retroduction.carma.core.api.testrunners.om.Mutant;
 import com.retroduction.carma.core.api.transitions.IMutationGenerator;
