9d33e6966d5a148d6a0945616d2e61c659fa8585
Alex Rau <alex@addresspush.com>
2008-02-23 18:51:14 +0000

    Removed unused imports.

--- Carma/Core/src/main/java/com/retroduction/carma/core/api/transitions/IMutationGenerator.java
+++ Carma/Core/src/main/java/com/retroduction/carma/core/api/transitions/IMutationGenerator.java
@@ -1,7 +1,6 @@
 package com.retroduction.carma.core.api.transitions;
 
 import java.util.List;
-import java.util.Set;
 
 import com.retroduction.carma.core.api.testrunners.om.Mutant;
 
--- Carma/Core/src/test/java/com/retroduction/carma/core/MutantGeneratorTestCase.java
+++ Carma/Core/src/test/java/com/retroduction/carma/core/MutantGeneratorTestCase.java
@@ -1,9 +1,7 @@
 package com.retroduction.carma.core;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import junit.framework.TestCase;
 
