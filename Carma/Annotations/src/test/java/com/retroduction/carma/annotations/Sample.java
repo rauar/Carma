9da5edccd1a2e6e686247c7dcd222e9f5187ba30
Alex Rau <alex@addresspush.com>
2008-04-01 16:33:50 +0000

    Added OSL headers

--- Carma/Annotations/src/test/java/com/retroduction/carma/annotations/Sample.java
+++ Carma/Annotations/src/test/java/com/retroduction/carma/annotations/Sample.java
@@ -1,3 +1,11 @@
+/**
+ *
+ *   Copyright Alexander Rau, Mike Groezinger, Retroduction.org - All rights reserved
+ *
+ *   This file is part of Carma. Carma is licensed under the OSL 3.0. The OSL 3.0 is
+ *   available here: http://www.opensource.org/licenses/osl-3.0.php
+ *
+ */
 package com.retroduction.carma.annotations;
 
 public class Sample {
--- Carma/Transformers/src/main/java/com/retroduction/carma/transformer/asm/aor/AOR_TransitionGroup.java
+++ Carma/Transformers/src/main/java/com/retroduction/carma/transformer/asm/aor/AOR_TransitionGroup.java
@@ -1,3 +1,11 @@
+/**
+ *
+ *   Copyright Alexander Rau, Mike Groezinger, Retroduction.org - All rights reserved
+ *
+ *   This file is part of Carma. Carma is licensed under the OSL 3.0. The OSL 3.0 is
+ *   available here: http://www.opensource.org/licenses/osl-3.0.php
+ *
+ */
 package com.retroduction.carma.transformer.asm.aor;
 
 import com.retroduction.carma.core.api.transitions.ITransition;
